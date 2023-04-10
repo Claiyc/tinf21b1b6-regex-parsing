0f7ad93c222a16d7920aaf158009bebbca2b1b0d
Claiyc <claiyc@outlook.com>
2023-04-10 17:59:58 +0200

    testing

--- src/main/java/de/dhbw/caput/tinf21b1b6/Parser.java
+++ src/main/java/de/dhbw/caput/tinf21b1b6/Parser.java
@@ -4,7 +4,18 @@ final class Parser {
 
 	public static RegularExpression parse( String string ){
 		// TODO: Customize the code here to create a regular expression from the given string.
+        // split at unions('|') & concatinations ('punkt')
+        // handle bracket depth
+        // recursive parse method calls
 		return new RegularExpression.EmptySet( );
 	}
 
+    private String inBracket(String str) {
+        for (final char c : str.toCharArray()) {
+            if (c == '(') {
+                // new opening bracket
+
+            }
+        }
+    }
 }
